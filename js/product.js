163ab26f15fe2e112060c31acf56b640d9cea1dd
KevinTruong72 <truongtuankhang.digital@gmail.com>
2025-01-16 19:52:10 +0700

    index.html contact.html intro.html contact.css intro.css policy.html policy.css

--- js/product.js
+++ js/product.js
@@ -3,7 +3,12 @@ const user = JSON.parse(localStorage.getItem("user"));
 const helloUser = document.getElementById("helloUser");
 
 if(user){
-    helloUser.innerHTML = `Hello ${user.username}`;
+    helloUser.innerHTML = `
+    <div class="d-flex align-items-center tenuser">
+    <i class="fa-solid fa-hand-holding-heart"></i>
+    <h5>${user.username}</h5>
+    </div>
+    `;
 } else{
     helloUser.innerHTML = `Hello Guest`;
 }
@@ -20,11 +25,12 @@ function showProduct(products) {
     for(let i = 0; i < products.length; i++){
         console.log(products[i].title);
         showData.innerHTML += `
-            <div class="card" style="width: 18rem;">
+            <div class="card mt-5" style="width: 18rem;">
     <img src="${products[i].Image}" alt="">
         <div class="card-body">
             <h5 class="card-title">${products[i].Name}</h5>
             <p class="card-text">${products[i].Price}</p>
+            <button class="btn btn-secondary nutmua" onclick="addToCart(${products[i].id})">Add to Cart</button>
         </div>
     </div>
         `
